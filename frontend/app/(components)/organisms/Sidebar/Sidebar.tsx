590eb832b6afcd95bee92bb85a51f76b99059f4d
rod <dev_rodts@outlook.com>
2025-03-11 17:26:45 -0300

    "feat: add sidebar component"

--- frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
+++ frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
+"use client"
+import { LockIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
 const Sidebar = () => {
+
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
 
@@ -18,11 +21,13 @@ const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-x
 
                 <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700 ">
                         <Image src="/logo.png" alt="logo" width={40} height={40} />
-                        <div className="text-xl font-bold text-gray-800 dark:text-white">
+                        <div className="text-md font-bold tracking-wide dark:text-gray-200">
                             RodTasks
                         </div>
+                        <div className="mt-1 flex items-start gap-2">
+                            <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400"/>
+                        </div>
                 </div>
-                
             </nav>
         </aside>
     )
