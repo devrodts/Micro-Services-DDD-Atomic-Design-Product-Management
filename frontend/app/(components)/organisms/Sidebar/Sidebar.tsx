8fa2364bc5934b53ae94c4fc3378a5441bb5aa47
rod <dev_rodts@outlook.com>
2025-03-11 15:21:01 -0300

    feat: init sidebar config

--- frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
+++ frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 
 const Sidebar = () => {
@@ -8,9 +9,20 @@ const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-x
     return(
         <aside className={sidebarClassNames}>
             <nav className="flex h-[100%] w-full flex-col justify-start">
+
                 <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
-                        
+                    <div className="text-xl font-bold text-gray-800 dark:text-white">
+                        RodTasks
+                    </div>
                 </div>
+
+                <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700 ">
+                        <Image src="/logo.png" alt="logo" width={40} height={40} />
+                        <div className="text-xl font-bold text-gray-800 dark:text-white">
+                            RodTasks
+                        </div>
+                </div>
+                
             </nav>
         </aside>
     )
