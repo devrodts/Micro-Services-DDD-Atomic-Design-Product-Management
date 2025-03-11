c9dc220267131b776b57bebbc45031a50afe0bbc
rod <dev_rodts@outlook.com>
2025-03-11 17:26:57 -0300

    "feat: add sidebar to the layout"

--- frontend/app/(components)/templates/DashboardWrapper/DashboardWrapper.tsx
+++ frontend/app/(components)/templates/DashboardWrapper/DashboardWrapper.tsx
@@ -1,10 +1,16 @@
 import Navbar from "../../organisms/Navbar/Navbar";
+import Sidebar from "../../organisms/Sidebar/Sidebar";
 
 const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
 
     return (
-        <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-            {/* sidebar */}
+        <div className="flex 
+            min-h-screen 
+            w-full 
+            bg-gray-50 
+            text-gray-900"
+        >
+            <Sidebar/>
             <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
                 <Navbar/>
                 {children}
