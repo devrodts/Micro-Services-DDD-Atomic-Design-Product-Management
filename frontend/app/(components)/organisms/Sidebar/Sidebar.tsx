17fc1878a9f961af00817baa5a570d812b9e5a58
rod <dev_rodts@outlook.com>
2025-03-12 07:14:59 -0300

    "refactor: refactor sidebar links"

--- frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
+++ frontend/app/(components)/organisms/Sidebar/Sidebar.tsx
@@ -19,15 +19,21 @@ const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-x
                     </div>
                 </div>
 
-                <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700 ">
+                <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+
                         <Image src="/logo.png" alt="logo" width={40} height={40} />
-                        <div className="text-md font-bold tracking-wide dark:text-gray-200">
-                            RodTasks
-                        </div>
+
+                        <div>
+                            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">RodTasks</h3>
                         <div className="mt-1 flex items-start gap-2">
                             <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400"/>
+                            <p className="text-xs text-gray-500">
+                                Private
+                            </p>
+                        </div>
                         </div>
                 </div>
+                {/* NAVBAR LINKS */}
             </nav>
         </aside>
     )
